0b21a328fae5f76db651a6efa745a7e87e16422f
jhrhq <jhr.code@proton.me>
2024-12-22 01:50:30 +0600

    feat: movie fetch

--- app/page.js
+++ app/page.js
@@ -4,8 +4,25 @@ import MovieCard from "@/components/landing/MovieCard";
 import Navbar from "@/components/Navbar";
 import { getAllWatchLists } from "@/db/queries";
 
+async function getData() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/movie/550?api_key=73740fc6ee836d15fc04346d7db75605",
+  );
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 export default async function Home() {
-  const watchListMovies = await getAllWatchLists();
+  // const watchListMovies = await getAllWatchLists();
+  const data = await getData();
+  // console.log(data);
   return (
     <>
       <Navbar />
