e00554e2e0a2ca4dfe06c143a46b888ebfa35080
johirhaqedipok <johirhaquedipok2@gmail.com>
2025-01-04 19:02:56 +0600

    refactor: duplicate pagination remvoed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import PropertyCard from "@/components/homepage/PropertyCard";
 import Navbar from "@/components/Navbar";
 import connectDB from "@/config/database";
 import { getAllProperties } from "@/db/queries";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 export default async function Home({
   searchParams: { page = 1, pageSize = 8 },
@@ -45,54 +44,6 @@ export default async function Home({
         />
       )}
 
-      <div className="mt-8 flex justify-center">
-        <nav aria-label="Page navigation">
-          <ul className="inline-flex items-center -space-x-px">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 ml-0 leading-tight text-zinc-500 bg-white rounded-l-lg border border-zinc-300 hover:bg-zinc-100 hover:text-zinc-700"
-              >
-                <span className="sr-only">Previous</span>
-                <FaChevronLeft className="fas fa-chevron-left" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-zinc-500 bg-white border border-zinc-300 hover:bg-zinc-100 hover:text-zinc-700"
-              >
-                1
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-zinc-500 bg-white border border-zinc-300 hover:bg-zinc-100 hover:text-zinc-700"
-              >
-                2
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="py-2 px-3 leading-tight text-zinc-500 bg-white border border-zinc-300 hover:bg-zinc-100 hover:text-zinc-700"
-              >
-                3
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 leading-tight text-zinc-500 bg-white rounded-r-lg border border-zinc-300 hover:bg-zinc-100 hover:text-zinc-700"
-              >
-                <span className="sr-only">Next</span>
-                <FaChevronRight className="fas fa-chevron-right" />
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
       <Footer />
     </>
   );
