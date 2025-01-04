7b60b08e27c9a34ecf229392d5691a4c926dc767
jhrhq <jhr.code@proton.me>
2025-01-04 21:44:33 +0600

    feat:search results, no results component

---
+++ components/homepage/NoResultsFound.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NoResultsFound = ({ searchTerm }: { searchTerm?: string }) => {
+  return (
+    <div className="container px-6 py-16 mx-auto text-center">
+      <div className="max-w-lg mx-auto">
+        <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
+          No Results found for{" "}
+          <span className="text-base">&quot;{searchTerm}&quot;</span>
+        </h1>
+        <p className="mt-6 text-gray-500 dark:text-gray-300 mb-5">
+          Please search with a better title of properties.
+        </p>
+        <Button asChild>
+          <Link href={"/"}>Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NoResultsFound;
--- db/queries.ts
+++ db/queries.ts
@@ -1,10 +1,28 @@
 import Property from "@/models/Property";
 
-async function getAllProperties(page, pageSize) {
+async function getAllProperties(
+  page: number,
+  pageSize: number,
+  search: string
+) {
   const skip = (page - 1) * pageSize;
   const total = await Property.countDocuments({});
-  const allProperties = await Property.find().skip(skip).limit(pageSize);
-  return { allProperties, total };
+
+  const titlePattern = new RegExp(search, "i");
+
+  const query = {
+    $or: [{ title: titlePattern }],
+  };
+
+  if (search) {
+    const total = await Property.countDocuments(query);
+    const allProperties = await Property.find(query).skip(skip).limit(pageSize);
+    return { allProperties, total };
+  } else {
+    const allProperties = await Property.find().skip(skip).limit(pageSize);
+    return { allProperties, total };
+  }
+  // const searchResult = convertToSerializableObject(searches)
 }
 
 export { getAllProperties };
