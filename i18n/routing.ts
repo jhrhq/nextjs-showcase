4b3fc257c5c365903503fcccfe5971ce8399c81b
jhrhq <jhr.code@proton.me>
2025-01-06 02:41:01 +0600

    refactor:switch langauge with search params

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -6,16 +6,14 @@ export const routing = defineRouting({
   // Use this locale when we can't match
   // another with our user's preferred locales
   // and when no locale is explicitly set.
-  defaultLocale: defaultLocale,
+  defaultLocale,
 
   // List all supported locales (en, bn).
-  locales: locales,
+  locales,
 
   // Automatic locale detection is enabled by
-  // default. We're disabling it to keep things
-  // simple for now. We'll enable it later when
-  // we cover locale detection.
-  localeDetection: false,
+  // default.
+  // localeDetection: true,
 });
 
 // Lightweight wrappers around Next.js' navigation APIs
