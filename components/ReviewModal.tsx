3c15c3b5de4ed2c119f5e08ba4689a2afa912da8
johir haque dipok <johirhaquedipok2@gmail.com>
2025-01-13 15:07:41 +0600

    refactor: review form validation with react hook form

--- components/ReviewModal.tsx
+++ components/ReviewModal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Rating } from "@/components/Rating";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,9 +10,61 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { FaStar, FaX } from "react-icons/fa6";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  PropertyReview,
+  ReviewInputSchema,
+} from "@/validationSchema/review-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { FaX } from "react-icons/fa6";
+
+const reviewFormDefaultValues = {
+  comment: "",
+  rating: 0,
+};
 
 const ReviewModal = () => {
+  const form = useForm<PropertyReview>({
+    resolver: zodResolver(ReviewInputSchema),
+    defaultValues: reviewFormDefaultValues,
+  });
+
+  async function onSubmit(values: PropertyReview) {
+    console.log(values);
+  }
+
+  // const handleOpenForm = async () => {
+  //   form.setValue('product', product._id)
+  //   form.setValue('user', userId!)
+  //   form.setValue('isBooked', true)
+
+  //   // setOpen(true)
+  // }
+  //   <FormField
+  //   control={form.control}
+  //   name='title'
+  //   render={({ field }) => (
+  //     <FormItem className='w-full'>
+  //       <FormLabel>{t('Title')}</FormLabel>
+  //       <FormControl>
+  //         <Input
+  //           placeholder={t('Enter title')}
+  //           {...field}
+  //         />
+  //       </FormControl>
+  //       <FormMessage />
+  //     </FormItem>
+  //   )}
+  // />
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -39,159 +92,68 @@ const ReviewModal = () => {
           <DialogDescription className="hidden">
             Review Description
           </DialogDescription>
-          <form className="space-y-6">
-            {/* Overall Rating */}
-            <div>
-              <label className="block text-gray-700 font-medium mb-2">
-                Overall Rating
-              </label>
-              <div className="flex gap-2">
-                <button
-                  type="button"
-                  className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-                >
-                  <FaStar className="fas fa-star" />
-                </button>
-                <button
-                  type="button"
-                  className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-                >
-                  <FaStar className="fas fa-star" />
-                </button>
-                <button
-                  type="button"
-                  className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-                >
-                  <FaStar className="fas fa-star" />
-                </button>
-                <button
-                  type="button"
-                  className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-                >
-                  <FaStar className="fas fa-star" />
-                </button>
-                <button
-                  type="button"
-                  className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-                >
-                  <FaStar className="fas fa-star" />
-                </button>
+          <Form {...form}>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="rating"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel className="text-gray-700 font-medium mb-2 text-base">
+                        Overall Rating
+                      </FormLabel>
+                      <FormControl>
+                        <Rating {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
               </div>
-            </div>
-            <div>
-              <label className="block text-gray-700 font-medium mb-2">
-                Your Review
-              </label>
-              <textarea
-                rows={4}
-                placeholder="Share your experience with other travelers..."
-                className="w-full px-4 py-3 rounded-lg border focus:border-gray-500 focus:ring-0 resize-none"
-                defaultValue={""}
-              />
-            </div>
-          </form>
+              <div>
+                <FormField
+                  control={form.control}
+                  name="comment"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel className="text-gray-700 font-medium mb-2 text-base">
+                        Your Review
+                      </FormLabel>
+                      <FormControl>
+                        <Textarea
+                          {...field}
+                          rows={4}
+                          placeholder="Share your experience with other travelers..."
+                          className="w-full px-4 text-base py-3 rounded-lg border focus:border-gray-500 focus:ring-0 resize-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="border-t pt-4 bg-gray-50">
+                <div className="flex justify-end gap-4">
+                  <DialogClose asChild>
+                    <Button
+                      className="px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-lg static"
+                      variant="ghost"
+                    >
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                  <Button className="px-4 py-2 bg-primary text-white rounded-lg hover:brightness-90">
+                    Submit Review
+                  </Button>
+                </div>
+              </DialogFooter>
+            </form>
+          </Form>
         </div>
-        <DialogFooter className="border-t pt-4 bg-gray-50">
-          <div className="flex justify-end gap-4">
-            <DialogClose asChild>
-              <Button
-                className="px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-lg static"
-                variant="ghost"
-              >
-                Cancel
-              </Button>
-            </DialogClose>
-            <Button className="px-4 py-2 bg-primary text-white rounded-lg hover:brightness-90">
-              Submit Review
-            </Button>
-          </div>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
 };
 
 export default ReviewModal;
-
-//  <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center">
-// <div className="bg-white rounded-2xl w-full max-w-xl mx-4 overflow-hidden">
-
-//   <div className="border-b p-4">
-//     <div className="flex justify-between items-center">
-//       <h3 className="text-xl font-semibold">Write a review</h3>
-//       <button className="text-gray-400 hover:text-gray-600">
-//         <i className="fas fa-times text-xl" />
-//       </button>
-//     </div>
-//   </div>
-
-//   <div className="p-6">
-//     <form className="space-y-6">
-
-//       <div>
-//         <label className="block text-gray-700 font-medium mb-2">
-//           Overall Rating
-//         </label>
-//         <div className="flex gap-2">
-//           <button
-//             type="button"
-//             className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-//           >
-//             <FaStar className="fas fa-star" />
-//           </button>
-//           <button
-//             type="button"
-//             className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-//           >
-//             <FaStar className="fas fa-star" />
-//           </button>
-//           <button
-//             type="button"
-//             className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-//           >
-//             <FaStar className="fas fa-star" />
-//           </button>
-//           <button
-//             type="button"
-//             className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-//           >
-//             <FaStar className="fas fa-star" />
-//           </button>
-//           <button
-//             type="button"
-//             className="text-2xl text-gray-300 hover:text-yellow-500 focus:text-yellow-500"
-//           >
-//             <FaStar className="fas fa-star" />
-//           </button>
-//         </div>
-//       </div>
-
-//       <div>
-//         <label className="block text-gray-700 font-medium mb-2">
-//           Your Review
-//         </label>
-//         <textarea
-//           rows={4}
-//           placeholder="Share your experience with other travelers..."
-//           className="w-full px-4 py-3 rounded-lg border focus:border-gray-500 focus:ring-0 resize-none"
-//           defaultValue={""}
-//         />
-//       </div>
-//     </form>
-//   </div>
-
-//   <div className="border-t p-4 bg-gray-50">
-//     <div className="flex justify-end gap-4">
-//       <button
-//         className="px-4 py-2 text-gray-600 hover:bg-gray-100 rounded-lg"
-
-//       >
-//         Cancel
-//       </button>
-//       <button className="px-4 py-2 bg-primary text-white rounded-lg hover:brightness-90">
-//         Submit Review
-//       </button>
-//     </div>
-//   </div>
-// </div>
-// </div>
--- validationSchema/review-schema.ts
+++ validationSchema/review-schema.ts
@@ -4,14 +4,16 @@ const MongoId = z
   .regex(/^[0-9a-fA-F]{24}$/, { message: "Invalid MongoDB ID" });
 
 export const ReviewInputSchema = z.object({
-  product: MongoId,
-  user: MongoId,
-  isBooked: z.boolean(),
-  title: z.string().min(1, "Title is required"),
-  comment: z.string().min(1, "Comment is required"),
+  // product: MongoId,
+  // user: MongoId,
+  // isBooked: z.boolean(),
+  // title: z.string().min(1, "Title is required"),
+  comment: z.string().min(1, "Review can not be empty"),
   rating: z.coerce
     .number()
     .int()
     .min(1, "Rating must be at least 1")
     .max(5, "Rating must be at most 5"),
 });
+
+export type PropertyReview = z.infer<typeof ReviewInputSchema>;
