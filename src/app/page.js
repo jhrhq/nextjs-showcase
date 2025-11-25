26fb70a1fc6bc3e26e3a236355f2bc577292c744
johirhaque <jhr.haq@gmail.com>
2025-11-26 02:44:24 +0600

    hits counter app using json as db

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
+import { readFile, writeFile } from "../helpers/file-helpers";
+
+
+const DATABASE_PATH = '/src/database.json';
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH))
+
+  hits += 1
+
+  writeFile(DATABASE_PATH, JSON.stringify({ hits }))
+
   return (
     <main>
-      <h1>Hello Next!</h1>
+      <h1>Welcome!</h1>
+      <h2>Hello Next!</h2>
+      <p>You are visitor number {hits}.</p>
       <footer>
         Page rendered on{' '}{
           new Date().toLocaleString()
