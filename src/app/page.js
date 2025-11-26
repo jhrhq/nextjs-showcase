7f1b91a8a69d8fa98e5fc265730aa62a8af080c5
johirhaque <jhr.haq@gmail.com>
2025-11-26 16:25:15 +0600

    hit counter in a independent component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { readFile, writeFile } from "../helpers/file-helpers";
+import HitCounter from "./hit-counter";
 
 
 const DATABASE_PATH = '/src/database.json';
@@ -14,7 +15,7 @@ function Home() {
     <main>
       <h1>Welcome!</h1>
       <h2>Hello Next!</h2>
-      <p>You are visitor number {hits}.</p>
+      <p>You are visitor number <HitCounter />.</p>
       <footer>
         Page rendered on{' '}{
           new Date().toLocaleString()
