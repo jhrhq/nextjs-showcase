a35dba970b77bbee4984ea831b4796f47c5b77ff
johirhaque <jhr.haq@gmail.com>
2025-11-27 20:08:08 +0600

    ondemand ssr

--- src/app/hit-counter.jsx
+++ src/app/hit-counter.jsx
@@ -2,6 +2,8 @@ import { readFile, writeFile } from "../helpers/file-helpers";
 
 const DATABASE_PATH = "/src/database.json";
 
+export const dynamic = "force-dynamic";
+
 function HitCounter() {
   let { hits } = JSON.parse(readFile(DATABASE_PATH));
 
