1556f787e1fea09a4a87f25b37b1a3c39459c31a
johirhaque <jhr.haq@gmail.com>
2025-11-28 01:08:27 +0600

    suspense loading on interview

---
+++ src/components/suspense/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import { Loader } from 'lucide-react';
+
+import styles from './Spinner.module.css';
+
+const Spinner = ({ color, size }) => {
+  return (
+    <div className={styles.wrapper}>
+      <Loader color={color} size={size} />
+    </div>
+  );
+};
+
+export default Spinner;
