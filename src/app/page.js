e3ad16c5f6136bd2504cb58e5c0101e1cddb4aa7
johirhaque <jhr.haq@gmail.com>
2025-12-02 05:16:44 +0600

    render blog posts from psot lists

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,24 @@
 
 
 import BlogSummaryCard from '@/components/blog/BlogSummaryCard';
+import { getBlogPostList } from '@/helpers/file-helpers';
 import styles from './homepage.module.css';
 
-function Home() {
+async function Home() {
+  const blogPosts = await getBlogPostList()
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>
         Latest Content:
       </h1>
 
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      {blogPosts.map(
+        ({ slug, ...delegated }) => <BlogSummaryCard
+          slug={slug}
+          {...delegated}
+        />
+      )
+      }
     </div>
   );
 }
