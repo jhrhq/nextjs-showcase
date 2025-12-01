e34dbfce4d75d31d9d59518ff25fe586602bbdb1
johirhaque <jhr.haq@gmail.com>
2025-12-02 00:57:31 +0600

    book selecting with animation

--- src/app/animation/page.js
+++ src/app/animation/page.js
@@ -1,4 +1,5 @@
 import AnimationMarginWrapper from "@/components/animation/animation-margin-wrapper"
+import BookPage from "@/components/animation/book"
 import CoinSorter from "@/components/animation/coinSorter"
 import FlexDemo from "@/components/animation/flex-demo"
 import ToggleRender from "@/components/animation/toggleRender"
@@ -33,6 +34,10 @@ export default function EnterAnimation() {
           numOfCoins={6}
         />
       </AnimationMarginWrapper>
+      <AnimationMarginWrapper title={'Book'} >
+        <BookPage />
+      </AnimationMarginWrapper>
+
       <div style={{ paddingBottom: 200 }}></div>
     </>
   )
---
+++ src/components/animation/book/reading-list.js
@@ -0,0 +1,53 @@
+'use client'
+import { X } from 'lucide-react';
+import { motion } from 'motion/react';
+
+import VisuallyHidden from '@/components/visually-hidden';
+import styles from './ReadingList.module.css';
+
+function ReadingList({ books, handleRemoveBook }) {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h2>Reading List</h2>
+        <ol className={styles.books}>
+          {books.map((book, bookIndex) => {
+            return (
+              <li key={book.isbn}>
+                <motion.img
+                  layoutId={`book-cover-${book.isbn}`}
+                  alt={book.name}
+                  src={book.coverSrc}
+                  draggable={false}
+                  className={styles.bookCover}
+                  transition={{
+                    type: 'spring',
+                    stiffness: 400,
+                    damping: 60,
+                  }}
+                />
+                <motion.button
+                  layout="position"
+                  className={styles.deleteBtn}
+                  onClick={() => handleRemoveBook(book)}
+                  transition={{
+                    type: 'spring',
+                    stiffness: 400,
+                    damping: 60,
+                  }}
+                >
+                  <X />
+                  <VisuallyHidden>
+                    Remove {book.name}
+                  </VisuallyHidden>
+                </motion.button>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default ReadingList;
