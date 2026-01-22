d69ce5b1f99879d72c0fef0a0f992d5ae43ad3a4
jhrhq <jhr.haq@gmail.com>
2026-01-22 20:00:48 +0600

    refactor(ui): independent page layout

--- src/app/(spa)/linker/layout.tsx
+++ src/app/(spa)/linker/layout.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import { Providers } from "./provider";
+import "../../globals.css";
+import { Toaster } from "@/components/ui/sonner";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Linker - SEO Link Management Tool",
@@ -11,5 +26,12 @@ export default async function LinkerLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <Providers>{children}</Providers>;
+  return (
+    <html lang="en" className={inter.variable}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
+        <Toaster closeButton={true} />
+      </body>
+    </html>
+  );
 }
