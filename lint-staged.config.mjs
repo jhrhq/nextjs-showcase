886341ee764d661300fab6d2a6fede6f56ddb369
jhrhq <jhr.haq@gmail.com>
2026-01-22 18:50:22 +0600

    chore: pre-commit, biome, package.json configured

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,11 @@
 export default {
-  "*.{js,jsx,ts,tsx}": [
+  "*.{js,jsx,ts,tsx,json,md,mdx}": [
     "biome check --write --no-errors-on-unmatched",
     "biome format --write --no-errors-on-unmatched",
+    () => "pnpm type-check"
   ],
-  "*.{json,md,mdx}": ["biome format --write --no-errors-on-unmatched"],
+  // "*.{json,md,mdx}": ["biome format --write --no-errors-on-unmatched"],
   // Run type-check after linting all files
-  "*.{ts,tsx}": () => "tsc --noEmit",
+  // "*.{ts,tsx}": () => "tsc --noEmit",
+
 };
