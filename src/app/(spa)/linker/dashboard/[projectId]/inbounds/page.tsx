47d28e35a24b10aff84c71aca184a887a6e3f8aa
jhrhq <jhr.haq@gmail.com>
2026-02-10 20:29:19 +0600

    wip(linker): inbounds page recommended suggestion

--- src/app/(spa)/linker/dashboard/[projectId]/inbounds/page.tsx
+++ src/app/(spa)/linker/dashboard/[projectId]/inbounds/page.tsx
@@ -1,11 +1,12 @@
-import InternalLinkBuilderPage from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/target-form";
+// import InternalLinkBuilderPage from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/target-form";
+import { InternalLinkBuilderPageOne } from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/test";
 /* import { InternalLinkBuilderPageOne } from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/test";
  */
 export default function InboundsPage() {
   return (
     <div className="text-5xl text-slate-900">
-      {/* <InternalLinkBuilderPageOne /> */}
-      <InternalLinkBuilderPage />
+      <InternalLinkBuilderPageOne />
+      {/* <InternalLinkBuilderPage /> */}
     </div>
   );
 }
