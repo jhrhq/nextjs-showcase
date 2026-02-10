3d47d89fae813ef9f82373e965767249745dbaa5
jhrhq <jhr.haq@gmail.com>
2026-02-11 03:10:14 +0600

    wip(linker): tiptap editor with anchor

--- src/app/(spa)/linker/dashboard/[projectId]/inbounds/page.tsx
+++ src/app/(spa)/linker/dashboard/[projectId]/inbounds/page.tsx
@@ -1,10 +1,14 @@
 // import InternalLinkBuilderPage from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/target-form";
+import SentenceEditor from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/SentenceEditorTiptap";
 import { InternalLinkBuilderPageOne } from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/test";
+import { NotesManager } from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/tiptap-editor-gpt-test";
 /* import { InternalLinkBuilderPageOne } from "@/app/(spa)/linker/dashboard/[projectId]/inbounds/test";
  */
 export default function InboundsPage() {
   return (
     <div className="text-5xl text-slate-900">
+      <NotesManager />
+      <SentenceEditor />
       <InternalLinkBuilderPageOne />
       {/* <InternalLinkBuilderPage /> */}
     </div>
