62b247aa3960d337ca5e7f1e1c5f54507484a5c0
jhrhq <jhr.haq@gmail.com>
2026-03-08 22:38:29 +0600

    chore: lint staged config udpated

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,10 @@ export default {
   "*.{js,jsx,ts,tsx,json,md,mdx}": [
     "biome check --write --no-errors-on-unmatched",
     "biome format --write --no-errors-on-unmatched",
-    () => "pnpm type-check"
+    // () => "pnpm type-check"
+  ],
+  "*.{ts,tsx}": [
+    () => "pnpm type-check",
   ],
   // "*.{json,md,mdx}": ["biome format --write --no-errors-on-unmatched"],
   // Run type-check after linting all files
