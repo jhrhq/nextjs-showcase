c514b9d690c0fcdc047afff9d4bfeb1f52f1ed8a
jhrhq <jhr.code@proton.me>
2026-05-17 18:31:26 +0600

    refactor(linker): custom network table loading & error component

--- src/app/(spa)/linker/dashboard/[projectId]/custom-network/[customNetworkId]/page.tsx
+++ src/app/(spa)/linker/dashboard/[projectId]/custom-network/[customNetworkId]/page.tsx
@@ -4,15 +4,16 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { useParams } from "next/navigation";
 import { db } from "@/domains/linker/db/indexdb";
 import InternalLinkManagement from "@/domains/linker/ui/custom-network/manage-custom-network-table/custom-network-table";
+import { InternalLinkManagementEmpty } from "@/domains/linker/ui/custom-network/manage-custom-network-table/custom-network-table-empty";
+import { InternalLinkManagementSkeleton } from "@/domains/linker/ui/custom-network/manage-custom-network-table/custom-network-table-skeleton";
 
 export default function CreateCustomNetworkTable() {
   const { projectId, customNetworkId } = useParams<{ projectId: string; customNetworkId: string }>();
   const data = useLiveQuery(() => db.customNetworks.get(projectId), [projectId]);
   const currentNetwork = data?.customNetworks.find((item) => item.id === customNetworkId);
 
-  if ((data || currentNetwork) === undefined) return <h1>Loading....</h1>;
-
-  if (!data || !currentNetwork) return <h1>No data</h1>;
+  if (data === undefined) return <InternalLinkManagementSkeleton />;
+  if (!currentNetwork) return <InternalLinkManagementEmpty />;
 
   return <InternalLinkManagement data={currentNetwork} />;
 }
