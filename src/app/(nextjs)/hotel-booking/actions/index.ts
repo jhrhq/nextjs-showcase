fb6ae5047c7e104e6146b445c309718556122de5
jhrhq <jhr.code@proton.me>
2026-06-11 07:33:15 +0600

    chore: unnecessary files removed and page title updated

--- src/app/(nextjs)/hotel-booking/actions/index.ts
+++
@@ -1,4 +0,0 @@
-import { googleSingIn } from "./googleLoginAction";
-import { loginFormAction } from "./loginAction";
-
-export { googleSingIn, loginFormAction };
--- src/app/(nextjs)/hotel-booking/api/auth/[...nextauth]/route.ts
+++
@@ -1,2 +0,0 @@
-import { handlers } from "@/domains/hotel-booking/auth";
-export const { GET, POST } = handlers;
