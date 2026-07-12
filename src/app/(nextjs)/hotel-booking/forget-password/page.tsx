7a5f1323ff43a71ab1a11ab93a4299563cf0d136
jhrhq <jhr.code@proton.me>
2026-07-12 07:38:39 +0600

    chore(hotel-booking): reaname forget page

--- src/app/(nextjs)/hotel-booking/forget-password/page.tsx
+++
@@ -1,7 +0,0 @@
-import ForgetPasswordForm from "@/domains/hotel-booking/components/auth/ForgetPasswordForm";
-
-const ForgetPassword = () => {
-  return <ForgetPasswordForm />;
-};
-
-export default ForgetPassword;
--- src/app/(nextjs)/hotel-booking/payment-success/page.tsx
+++ src/app/(nextjs)/hotel-booking/payment-success/page.tsx
@@ -1,4 +1,4 @@
-import { CircleCheck, Download, Luggage, Mail, MessageSquare, MessageSquareMore, Star } from "lucide-react";
+import { CircleCheck, Download, Luggage, Mail, MessageSquareMore, Star } from "lucide-react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -20,11 +20,11 @@ const PaymentSuccess = () => {
       {/* Booking Details Card */}
       <div className="bg-white rounded-lg shadow-sm p-6 mb-8">
         <div className="flex items-start gap-6 mb-6 pb-6 border-b">
-          <img
+          {/*<img
             src="https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Property"
             className="w-32 h-32 rounded-lg object-cover"
-          />
+          />*/}
           <div>
             <h2 className="text-2xl font-semibold mb-2">Sea View Room</h2>
             <div className="flex items-center mb-2">
