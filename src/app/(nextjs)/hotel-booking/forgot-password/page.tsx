cce1e6effba5c036edb61551f08b5af899e7aaf8
jhrhq <jhr.code@proton.me>
2026-07-12 07:47:37 +0600

    chore(hotel-booking): renamed forgotPassword page

---
+++ src/app/(nextjs)/hotel-booking/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+import ForgetPasswordForm from "@/domains/hotel-booking/components/auth/ForgetPasswordForm";
+
+const ForgetPassword = () => {
+  return <ForgetPasswordForm />;
+};
+
+export default ForgetPassword;
