5b89f81c7d510e45bee5de68f97df21b91bcc15b
jhrhq <jhr.code@proton.me>
2026-08-16 06:21:50 +0600

    refactor: removed mode toggle from hotel-booking, added replaced static route with dynamic constant

--- src/domains/hotel-booking/components/navbar/index.tsx
+++ src/domains/hotel-booking/components/navbar/index.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import Search from "@/domains/hotel-booking/components/navbar/Search";
 import UserDropdownMenu from "@/domains/hotel-booking/components/navbar/UserDropdownMenu";
-import { ModeToggle } from "@/ui/shared/theme-toggle";
+import { AUTH_CONFIG } from "../../constants/auth.constants";
 
 const Navbar = async ({ showSearch }: { showSearch: boolean }) => {
   return (
     <nav className="grid grid-cols-2 md:flex justify-between items-center py-3 bg-white border-b mb-6 md:gap-8 px-4 md:px-8 lg:px-20">
       <div className="flex items-center shrink-0">
-        <Link href="/hotel-booking">
+        <Link href={AUTH_CONFIG.ROUTES.HOME}>
           <Image
             loading="eager"
             src="/hotel-booking-logo.svg"
@@ -24,7 +24,6 @@ const Navbar = async ({ showSearch }: { showSearch: boolean }) => {
 
       <div className="flex items-center space-x-4 relative justify-end">
         <UserDropdownMenu />
-        <ModeToggle />
       </div>
     </nav>
   );
